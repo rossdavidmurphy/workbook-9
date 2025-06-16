6871c88dab5034c3ec0d1f051b640a2b275dbc52
Ross Murphy <rossdavidmurphy@gmail.com>
2025-06-16 15:33:03 -0400

    Northwind SpringBoot setup.

---
+++ NorthwindTradersSpringBoot/src/test/java/com/pluarlsight/NorthwindTradersSpringBoot/NorthwindTradersSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pluarlsight.NorthwindTradersSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NorthwindTradersSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
